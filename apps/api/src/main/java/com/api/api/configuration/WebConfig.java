13cff22d6df5c9d984229e4e2aefb6c25e97b44e
Juan Pablo Flores Díaz <101837425+JuanPabloFloresDiaz@users.noreply.github.com>
2025-06-10 00:09:01 -0600

    ✨ Añadir configuración de CORS y conversores de mensajes en WebConfig

---
+++ apps/api/src/main/java/com/api/api/configuration/WebConfig.java
@@ -0,0 +1,45 @@
+package com.api.api.configuration;
+
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value; // Importar @Value
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Value("${app.cors.allowed-origins}")
+    private String[] allowedOrigins;
+
+    @SuppressWarnings("null")
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(allowedOrigins)
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+
+    @SuppressWarnings("null")
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        converters.stream()
+                .filter(converter -> converter instanceof MappingJackson2HttpMessageConverter)
+                .forEach(converter -> {
+                    MappingJackson2HttpMessageConverter jsonConverter = (MappingJackson2HttpMessageConverter) converter;
+                    List<MediaType> mediaTypes = new ArrayList<>();
+                    mediaTypes.add(MediaType.APPLICATION_JSON);
+                    mediaTypes.add(new MediaType("application", "json", StandardCharsets.UTF_8));
+                    jsonConverter.setSupportedMediaTypes(mediaTypes);
+                });
+    }
+}
