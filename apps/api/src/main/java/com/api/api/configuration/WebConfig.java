1cd1298e3e51a15a2c092033d9cae04e0a468a5f
Juan Pablo Flores Díaz <101837425+JuanPabloFloresDiaz@users.noreply.github.com>
2025-06-10 00:48:37 -0600

    ✨ Añadir configuración de dependencias en el proyecto frontend.

--- apps/api/src/main/java/com/api/api/configuration/WebConfig.java
+++ apps/api/src/main/java/com/api/api/configuration/WebConfig.java
@@ -3,8 +3,7 @@ package com.api.api.configuration;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
-
-import org.springframework.beans.factory.annotation.Value; // Importar @Value
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
