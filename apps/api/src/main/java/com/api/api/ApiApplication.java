48d593a765022e2ba5cd1faaadcef386ad729f16
Juan Pablo Flores Díaz <101837425+JuanPabloFloresDiaz@users.noreply.github.com>
2025-06-12 23:47:16 -0600

    ✨ Cargar variables del archivo .env al iniciar la aplicación

--- apps/api/src/main/java/com/api/api/ApiApplication.java
+++ apps/api/src/main/java/com/api/api/ApiApplication.java
@@ -1,12 +1,19 @@
 package com.api.api;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+@EnableJpaAuditing
 @SpringBootApplication
 public class ApiApplication {
 
 	public static void main(String[] args) {
+		// Cargar variables del archivo .env
+        Dotenv dotenv = Dotenv.configure().load();
+        dotenv.entries().forEach(entry -> {
+            System.setProperty(entry.getKey(), entry.getValue());
+        });
 		SpringApplication.run(ApiApplication.class, args);
 	}
 
