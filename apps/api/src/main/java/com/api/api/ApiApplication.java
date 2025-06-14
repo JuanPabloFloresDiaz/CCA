e1c1ecfb576cfc4c8a19bd215202947bc29ab770
Juan Pablo Flores Díaz <101837425+JuanPabloFloresDiaz@users.noreply.github.com>
2025-06-14 07:54:54 -0600

    ✨ Habilitar soporte para AOP en la aplicación al añadir la anotación @EnableAspectJAutoProxy en ApiApplication.

--- apps/api/src/main/java/com/api/api/ApiApplication.java
+++ apps/api/src/main/java/com/api/api/ApiApplication.java
@@ -3,9 +3,11 @@ package com.api.api;
 import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableJpaAuditing
 @SpringBootApplication
+@EnableAspectJAutoProxy
 public class ApiApplication {
 
 	public static void main(String[] args) {
