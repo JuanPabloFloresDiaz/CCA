f04c67eed45f000e670bbe46438487bc707e1a38
Juan Pablo Flores Díaz <101837425+JuanPabloFloresDiaz@users.noreply.github.com>
2025-06-22 22:42:11 -0600

    ✨ Refactor acciones de auditoría en AplicacionesService para utilizar constantes de AuditActions

--- apps/api/src/main/java/com/api/api/service/AplicacionesService.java
+++ apps/api/src/main/java/com/api/api/service/AplicacionesService.java
@@ -5,6 +5,7 @@ import com.api.api.dto.SimpleDTO.AplicacionSimpleDTO;
 import com.api.api.model.Aplicaciones;
 import com.api.api.audit.AuditableAction;
 import com.api.api.audit.AuditableAction.AuditResultType;
+import com.api.api.audit.AuditActions;
 
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
@@ -23,14 +24,14 @@ public class AplicacionesService {
     }
     
     // Auditar la acción de búsqueda de todas las aplicaciones
-    @AuditableAction(actionName = "Búsqueda de Aplicaciones", message = "Se intentó buscar todas las aplicaciones.", auditResult = AuditResultType.BOTH)
+    @AuditableAction(actionName = AuditActions.BUSQUEDA_APLICACIONES_SIMPLE, message = "Se intentó buscar todas las aplicaciones.", auditResult = AuditResultType.BOTH)
     // Buscar todas las aplicaciones sin paginación ni búsqueda, solo id y nombre
     public Iterable<AplicacionSimpleDTO> findAllSelect() {
         return aplicacionesRepository.findAllSelect();
     }
 
     // Auditar la acción de búsqueda de todas las aplicaciones con paginación y búsqueda opcional
-    @AuditableAction(actionName = "Búsqueda de Aplicaciones con Paginación", message = "Se intentó buscar todas las aplicaciones con paginación y búsqueda opcional.", auditResult = AuditResultType.BOTH)
+    @AuditableAction(actionName = AuditActions.BUSQUEDA_APLICACIONES, message = "Se intentó buscar todas las aplicaciones con paginación y búsqueda opcional.", auditResult = AuditResultType.BOTH)
     // Buscar todas las aplicaciones con paginación y búsqueda opcional
     public Page<Aplicaciones> findAll(int page, int limit, String searchTerm) {
         Pageable pageable = PageRequest.of(page - 1, limit);
@@ -41,21 +42,21 @@ public class AplicacionesService {
     }
 
     // Auditar la acción de búsqueda de una aplicación por su ID
-    @AuditableAction(actionName = "Búsqueda de Aplicación por ID", message = "Se intentó buscar una aplicación por su ID.", auditResult = AuditResultType.BOTH)
+    @AuditableAction(actionName = AuditActions.BUSQUEDA_APLICACIONES, message = "Se intentó buscar una aplicación por su ID.", auditResult = AuditResultType.BOTH)
     // Buscar una aplicación por su ID
     public Optional<Aplicaciones> findById(UUID id) {
         return aplicacionesRepository.findById(id);
     }
 
     // Auditar la acción de creación de una aplicación
-    @AuditableAction(actionName = "Creación de Aplicación", message = "Se intentó crear una nueva aplicación.")
+    @AuditableAction(actionName = AuditActions.CREACION_APLICACION, message = "Se intentó crear una nueva aplicación.")
     // Crear una nueva aplicación
     public Aplicaciones create(Aplicaciones aplicacion) {
         return aplicacionesRepository.save(aplicacion);
     }
 
     // Auditar la acción de creación de múltiples aplicaciones
-    @AuditableAction(actionName = "Creación de Múltiples Aplicaciones", message = "Se intentó crear múltiples aplicaciones.")
+    @AuditableAction(actionName = AuditActions.CREACION_APLICACION, message = "Se intentó crear múltiples aplicaciones.")
     // Actualizar una aplicación existente
     public Optional<Aplicaciones> update(UUID id, Aplicaciones aplicacionActualizada) {
         return aplicacionesRepository.findById(id).map(aplicacion -> {
@@ -67,14 +68,14 @@ public class AplicacionesService {
         });
     }
     // Auditar la acción de eliminación de una aplicación por su ID
-    @AuditableAction(actionName = "Eliminación de Aplicación por ID", message = "Se intentó eliminar una aplicación por su ID.")
+    @AuditableAction(actionName = AuditActions.ELIMINACION_DEFINITIVA_APLICACION, message = "Se intentó eliminar una aplicación por su ID.")
     // Eliminar definitivamente una aplicación por su ID
     public void deleteById(UUID id) {
         aplicacionesRepository.deleteById(id);
     }
 
     // Auditar la acción de eliminación lógica de una aplicación por su ID
-    @AuditableAction(actionName = "Eliminación Lógica de Aplicación", message = "Se intentó eliminar lógicamente una aplicación por su ID.")
+    @AuditableAction(actionName = AuditActions.ELIMINACION_LOGICA_APLICACION, message = "Se intentó eliminar lógicamente una aplicación por su ID.")
     // Eliminar lógicamente una aplicación por su ID
     public Optional<Aplicaciones> softDelete(UUID id) {
         return aplicacionesRepository.findById(id).map(aplicacion -> {
@@ -84,7 +85,7 @@ public class AplicacionesService {
     }
 
     // Auditar la acción de busqueda de aplicaciones por estado con paginación
-    @AuditableAction(actionName = "Búsqueda de Aplicaciones por Estado", message = "Se intentó buscar aplicaciones por estado con paginación.", auditResult = AuditResultType.BOTH)
+    @AuditableAction(actionName = AuditActions.BUSQUEDA_APLICACIONES, message = "Se intentó buscar aplicaciones por estado con paginación.", auditResult = AuditResultType.BOTH)
     // Filtrar aplicaciones por estado con paginación
     public Page<Aplicaciones> findByEstado(String estado, int page, int limit) {
         Pageable pageable = PageRequest.of(page - 1, limit);
