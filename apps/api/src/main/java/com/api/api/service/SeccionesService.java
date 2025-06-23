4c137bec56d854cbf6d707832b87de9ac8df30ea
Juan Pablo Flores Díaz <101837425+JuanPabloFloresDiaz@users.noreply.github.com>
2025-06-22 22:42:31 -0600

    ✨ Refactor SeccionesService para utilizar constantes de AuditActions en lugar de cadenas de texto para las acciones de auditoría.

--- apps/api/src/main/java/com/api/api/service/SeccionesService.java
+++ apps/api/src/main/java/com/api/api/service/SeccionesService.java
@@ -6,6 +6,7 @@ import com.api.api.model.Secciones;
 
 import com.api.api.audit.AuditableAction;
 import com.api.api.audit.AuditableAction.AuditResultType;
+import com.api.api.audit.AuditActions;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.data.domain.Page;
@@ -24,7 +25,7 @@ public class SeccionesService {
     }
 
     // Auditar la acción de búsqueda de todas las secciones
-    @AuditableAction(actionName = "Búsqueda de Secciones", message = "Se intentó buscar todas las secciones.", auditResult = AuditResultType.BOTH)
+    @AuditableAction(actionName = AuditActions.BUSQUEDA_SECCIONES_SIMPLE, message = "Se intentó buscar todas las secciones.", auditResult = AuditResultType.BOTH)
     // Buscar todas las secciones sin paginación ni búsqueda y trayendo solo el id y
     // el nombre para evitar problemas de rendimiento
     public Iterable<SeccionSimpleDTO> findAllSelect() {
@@ -34,7 +35,7 @@ public class SeccionesService {
     // Auditar la acción de búsqueda de todas las secciones con paginación y
     // búsqueda
     // opcional
-    @AuditableAction(actionName = "Búsqueda de Secciones con Paginación", message = "Se intentó buscar todas las secciones con paginación y búsqueda opcional.", auditResult = AuditResultType.BOTH)
+    @AuditableAction(actionName = AuditActions.BUSQUEDA_SECCIONES, message = "Se intentó buscar todas las secciones con paginación y búsqueda opcional.", auditResult = AuditResultType.BOTH)
     // Buscar todas las secciones con paginación y búsqueda opcional
     public Page<Secciones> findAll(int page, int limit, String searchTerm) {
         Pageable pageable = PageRequest.of(page - 1, limit);
@@ -45,28 +46,28 @@ public class SeccionesService {
     }
 
     // Auditar la acción de búsqueda de una sección por su ID
-    @AuditableAction(actionName = "Búsqueda de Sección por ID", message = "Se intentó buscar una sección por su ID.", auditResult = AuditResultType.BOTH)
+    @AuditableAction(actionName = AuditActions.BUSQUEDA_SECCIONES, message = "Se intentó buscar una sección por su ID.", auditResult = AuditResultType.BOTH)
     // Buscar una sección por su ID
     public Optional<Secciones> findById(UUID id) {
         return seccionesRepository.findById(id);
     }
 
     // Auditar la acción de creación de una sección
-    @AuditableAction(actionName = "Creación de Sección", message = "Se intentó crear una nueva sección.")
+    @AuditableAction(actionName = AuditActions.CREACION_SECCION, message = "Se intentó crear una nueva sección.")
     // Crear una nueva sección
     public Secciones create(Secciones seccion) {
         return seccionesRepository.save(seccion);
     }
 
     // Auditar la acción de creación de múltiples secciones
-    @AuditableAction(actionName = "Creación de Múltiples Secciones", message = "Se intentó crear múltiples secciones.")
+    @AuditableAction(actionName = AuditActions.CREACION_SECCION, message = "Se intentó crear múltiples secciones.")
     // Crear múltiples secciones
     public Iterable<Secciones> createAll(Iterable<Secciones> secciones) {
         return seccionesRepository.saveAll(secciones);
     }
 
     // Auditar la acción de actualización de una sección
-    @AuditableAction(actionName = "Actualización de Sección", message = "Se intentó actualizar una sección.")
+    @AuditableAction(actionName = AuditActions.ACTUALIZACION_SECCION, message = "Se intentó actualizar una sección.")
     // Actualizar una sección existente
     public Optional<Secciones> update(UUID id, Secciones seccionActualizada) {
         return seccionesRepository.findById(id).map(seccion -> {
@@ -77,14 +78,14 @@ public class SeccionesService {
     }
 
     // Auditar la acción de eliminación de una sección por su ID
-    @AuditableAction(actionName = "Eliminación de Sección por ID", message = "Se intentó eliminar una sección por su ID.")
+    @AuditableAction(actionName = AuditActions.ELIMINACION_DEFINITIVA_SECCION, message = "Se intentó eliminar una sección por su ID.")
     // Eliminar definitivamente una sección por su ID
     public void deleteById(UUID id) {
         seccionesRepository.deleteById(id);
     }
 
     // Auditar la acción de eliminación lógica de una sección por su ID
-    @AuditableAction(actionName = "Eliminación Lógica de Sección por ID", message = "Se intentó eliminar lógicamente una sección por su ID.")
+    @AuditableAction(actionName = AuditActions.ELIMINACION_LOGICA_SECCION, message = "Se intentó eliminar lógicamente una sección por su ID.")
     // Eliminar lógicamente una sección por su ID
     public Optional<Secciones> softDelete(UUID id) {
         return seccionesRepository.findById(id).map(seccion -> {
